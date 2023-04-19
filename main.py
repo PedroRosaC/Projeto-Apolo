88f2bf63faf51e570c479511044ddbea665e4eca
PedroRosaC <103642069+PedroRosaC@users.noreply.github.com>
2023-04-19 15:42:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+a = int(input('primeiro número'))
+b = int(input('Segundo número'))
+print('resultado =',a+b)
+
+
+if (a+b==2 and a == 1) or (a+b == 3 and b == 1):
+    print('OIII')
+elif a+b == 3: 
+    print('OLAA')
+
+cores = ['azul', 'vermelho', 'verde']
+opcao = int(input('boa noite vadia'))
+
+
+while opcao != 3: 
+    print('PORRA DE CODIGO FDP SEMATA')
+    opcao += 1 ;
+for cor in cores:
+    print(cor)
+
